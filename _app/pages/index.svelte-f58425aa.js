a60e6c3b6b08705ae569da629998d6b3ddf5e22c
Will McVay <wmcvay@reapit.com>
2021-10-10 14:07:49 +0100

    Updates

---
+++ _app/pages/index.svelte-f58425aa.js
@@ -0,0 +1,265 @@
+import{S as R,i as j,s as C,e as u,t as _,k as q,c as f,a as h,g as k,d as r,n as E,b as g,f as H,H as a,I as $,j as F,J as I,m as M,o as P,x as A,u as N,v as W}from"../chunks/vendor-e0b44dad.js";function z(x){let t,i,d,s,e,c,n,o,l,p,b,T;return{c(){t=u("div"),i=u("h2"),d=_("Frog & Wolf PR"),s=q(),e=u("h1"),c=_("Coming Soon..."),n=q(),o=u("form"),l=u("input"),p=q(),b=u("button"),T=_("Tell me more!"),this.h()},l(v){t=f(v,"DIV",{class:!0});var m=h(t);i=f(m,"H2",{});var S=h(i);d=k(S,"Frog & Wolf PR"),S.forEach(r),s=E(m),e=f(m,"H1",{});var w=h(e);c=k(w,"Coming Soon..."),w.forEach(r),n=E(m),o=f(m,"FORM",{});var y=h(o);l=f(y,"INPUT",{type:!0,name:!0,id:!0,placeholder:!0}),p=E(y),b=f(y,"BUTTON",{type:!0});var L=h(b);T=k(L,"Tell me more!"),L.forEach(r),y.forEach(r),m.forEach(r),this.h()},h(){g(l,"type","email"),g(l,"name","email"),g(l,"id","email-input"),g(l,"placeholder","Enter your email addressq"),g(b,"type","submit"),g(t,"class","container svelte-1020avt")},m(v,m){H(v,t,m),a(t,i),a(i,d),a(t,s),a(t,e),a(e,c),a(t,n),a(t,o),a(o,l),a(o,p),a(o,b),a(b,T)},p:$,i:$,o:$,d(v){v&&r(t)}}}class O extends R{constructor(t){super();j(this,t,null,z,C,{})}}function Q(x){let t,i,d,s,e,c;return e=new O({}),{c(){t=u("style"),i=_(`/* http://meyerweb.com/eric/tools/css/reset/ 
+   v2.0 | 20110126
+   License: none (public domain)
+*/
+
+		html,
+		body,
+		div,
+		span,
+		applet,
+		object,
+		iframe,
+		h1,
+		h2,
+		h3,
+		h4,
+		h5,
+		h6,
+		p,
+		blockquote,
+		pre,
+		a,
+		abbr,
+		acronym,
+		address,
+		big,
+		cite,
+		code,
+		del,
+		dfn,
+		em,
+		img,
+		ins,
+		kbd,
+		q,
+		s,
+		samp,
+		small,
+		strike,
+		strong,
+		sub,
+		sup,
+		tt,
+		var,
+		b,
+		u,
+		i,
+		center,
+		dl,
+		dt,
+		dd,
+		ol,
+		ul,
+		li,
+		fieldset,
+		form,
+		label,
+		legend,
+		table,
+		caption,
+		tbody,
+		tfoot,
+		thead,
+		tr,
+		th,
+		td,
+		article,
+		aside,
+		canvas,
+		details,
+		embed,
+		figure,
+		figcaption,
+		footer,
+		header,
+		hgroup,
+		menu,
+		nav,
+		output,
+		ruby,
+		section,
+		summary,
+		time,
+		mark,
+		audio,
+		video {
+			margin: 0;
+			padding: 0;
+			border: 0;
+			font-size: 100%;
+			font: inherit;
+			vertical-align: baseline;
+		}
+		/* HTML5 display-role reset for older browsers */
+		article,
+		aside,
+		details,
+		figcaption,
+		figure,
+		footer,
+		header,
+		hgroup,
+		menu,
+		nav,
+		section {
+			display: block;
+		}
+		body {
+			line-height: 1;
+		}
+		ol,
+		ul {
+			list-style: none;
+		}
+		blockquote,
+		q {
+			quotes: none;
+		}
+		blockquote:before,
+		blockquote:after,
+		q:before,
+		q:after {
+			content: '';
+			content: none;
+		}
+		table {
+			border-collapse: collapse;
+			border-spacing: 0;
+		}
+
+		* {
+			font-family: 'Quicksand', Helvetica, Arial, sans-serif;
+		}`),d=q(),s=u("main"),F(e.$$.fragment),this.h()},l(n){const o=I('[data-svelte="svelte-19hvvei"]',document.head);t=f(o,"STYLE",{});var l=h(t);i=k(l,`/* http://meyerweb.com/eric/tools/css/reset/ 
+   v2.0 | 20110126
+   License: none (public domain)
+*/
+
+		html,
+		body,
+		div,
+		span,
+		applet,
+		object,
+		iframe,
+		h1,
+		h2,
+		h3,
+		h4,
+		h5,
+		h6,
+		p,
+		blockquote,
+		pre,
+		a,
+		abbr,
+		acronym,
+		address,
+		big,
+		cite,
+		code,
+		del,
+		dfn,
+		em,
+		img,
+		ins,
+		kbd,
+		q,
+		s,
+		samp,
+		small,
+		strike,
+		strong,
+		sub,
+		sup,
+		tt,
+		var,
+		b,
+		u,
+		i,
+		center,
+		dl,
+		dt,
+		dd,
+		ol,
+		ul,
+		li,
+		fieldset,
+		form,
+		label,
+		legend,
+		table,
+		caption,
+		tbody,
+		tfoot,
+		thead,
+		tr,
+		th,
+		td,
+		article,
+		aside,
+		canvas,
+		details,
+		embed,
+		figure,
+		figcaption,
+		footer,
+		header,
+		hgroup,
+		menu,
+		nav,
+		output,
+		ruby,
+		section,
+		summary,
+		time,
+		mark,
+		audio,
+		video {
+			margin: 0;
+			padding: 0;
+			border: 0;
+			font-size: 100%;
+			font: inherit;
+			vertical-align: baseline;
+		}
+		/* HTML5 display-role reset for older browsers */
+		article,
+		aside,
+		details,
+		figcaption,
+		figure,
+		footer,
+		header,
+		hgroup,
+		menu,
+		nav,
+		section {
+			display: block;
+		}
+		body {
+			line-height: 1;
+		}
+		ol,
+		ul {
+			list-style: none;
+		}
+		blockquote,
+		q {
+			quotes: none;
+		}
+		blockquote:before,
+		blockquote:after,
+		q:before,
+		q:after {
+			content: '';
+			content: none;
+		}
+		table {
+			border-collapse: collapse;
+			border-spacing: 0;
+		}
+
+		* {
+			font-family: 'Quicksand', Helvetica, Arial, sans-serif;
+		}`),l.forEach(r),o.forEach(r),d=E(n),s=f(n,"MAIN",{});var p=h(s);M(e.$$.fragment,p),p.forEach(r),this.h()},h(){document.title="Frog & Wolf PR"},m(n,o){a(document.head,t),a(t,i),H(n,d,o),H(n,s,o),P(e,s,null),c=!0},p:$,i(n){c||(A(e.$$.fragment,n),c=!0)},o(n){N(e.$$.fragment,n),c=!1},d(n){r(t),n&&r(d),n&&r(s),W(e)}}}class B extends R{constructor(t){super();j(this,t,null,Q,C,{})}}export{B as default};
